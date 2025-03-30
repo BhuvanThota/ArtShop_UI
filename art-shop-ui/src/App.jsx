0b0ae8534178f343eadf5555f9e7c4a05c3775ab
BhuvanThota <teacherb3337@gmail.com>
2025-03-30 16:06:57 +0530

    Day-1

--- art-shop-ui/src/App.jsx
+++ art-shop-ui/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   const [like, setLike] = useState(0);
 
   return (
     <>
+      <Header/>
+      
       <div className='flex-col justify-items-center'>
         <h1 className='text-3xl '>ART SHOP</h1>
         <button onClick={() => setLike(like+1)} className='bg-blue-500 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-emerald-500 rounded-3xl text-xl p-4 mt-4'>Click to Like: {like}</button>
       </div>
 
+      <Footer/>
     </>
   )
 }
---
+++ art-shop-ui/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Hero from './Hero';
+
+export function Header() {
+    return (
+        <>
+            <Navbar/>
+            <Hero/>                
+        </>
+    )
+}
+
+
+
